8eed2982434c81aa40130723c7c053069f2a67a3
Ken Chen <ken.chen@cheproximity.com.au>
2021-12-03 09:30:32 +1100

    Added dummy  AuthService

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import { User } from '../model/Model'
+import { AuthService } from '../services/AuthService'
 
-function App() {
-  return (
-   <div>App works</div>
-  );
+
+interface AppState{
+  user: User | undefined
+
 }
+
+export class App extends React.Component<{},AppState>{
+  
+  private authService: AuthService = new AuthService();
+
+  render(){
+    return(
+      <div>App works from class works !!!!</div>
+    )
+  }
+}
+
+
+
+// function App() {
+//   return (
+//    <div>App works</div>
+//   );
+// }
 
 export default App;
